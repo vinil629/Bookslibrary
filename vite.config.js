8127a5f30383c78bb528594ada37d1527a8e8b91
vinil629 <reddyreddyvinilavr@gmail.com>
2025-06-04 15:31:27 +0530

    changes occur in vite

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/Bookslibrary/',
   plugins: [react(),
     tailwindcss(),
    
   ],
-   base: '/Bookslibrary/',
+  
 })
